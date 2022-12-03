ada5733f3f31c2075d950a507f95a9f7925ad6dd
Manjeet Pandey <075bct048.manjeet@pacmpus.edu.np>
2022-12-04 05:33:40 +0545

    feedback form connected

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'data_collection',
     'data_annotation',
-    'ocr_tesseract',
     'feedback',
     'rest_framework',
     'corsheaders'
